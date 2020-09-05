6bb64ab3eee8a704efcf723bd0906609c9537666
Abhay Kumar <Abhaykumar30495@gmail.com>
2020-09-06 00:43:14 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -6,16 +6,16 @@ class BuildResume{
 		let i = parseInt(document.getElementsByClassName(className)[0].childElementCount);
 
 		if(className =='cert-div' && i <6){
-			let classtoAdd = ['form-group','col-md-6','col-sm-6'];
+			let classtoAdd = ['form-group','col-md-6','col-sm-12'];
 			childDiv.classList.add(...classtoAdd);
-			childDiv.innerHTML=`<label>${i+1}</label><input type='text' class='form-control'>`
+			childDiv.innerHTML=`<label>${i+1}</label><input type='text' class='form-control' id='txtOther${i+1}'>`
 
 		}else if(className =='skill-div' && i<12){
-			let classtoAdd = ['form-group','col-md-3','col-sm-3'];
+			let classtoAdd = ['form-group','col-md-3','col-sm-12'];
 			childDiv.classList.add(...classtoAdd);
 			
 			childDiv.id=`skill${i+1}`;
-			childDiv.innerHTML=`<label>Skills ${i+1}</label><span class="icon-remove"><a class='glyphicon glyphicon-remove-sign'></a><input type='text' class='form-control' id=''></span>`;
+			childDiv.innerHTML=`<label>Skills ${i+1}</label><span class="icon-remove"><a class='glyphicon glyphicon-remove-sign'></a><input type='text' class='form-control' id='txtskill${i+1}'></span>`;
 
 		}else{
 			alert('Sorry! You cannot add more , currently this website is in Beta Stage')
@@ -44,7 +44,7 @@ function delElm(className){
 	elem.del(className);
 }
 
-
+//Event Delegation - for deletion of skills
 document.querySelector('.skill-div').addEventListener('click', (e) =>{
 	if(e.target.tagName=="A"){
 		// console.log(e.path[2].id);
@@ -56,7 +56,61 @@ document.querySelector('.skill-div').addEventListener('click', (e) =>{
 });
 
 
+//For Saving Data
 
+function SaveData(){
+
+	let htmlids = document.querySelectorAll('*[id]');
+	let arrOfId = Array.prototype.map.call( htmlids, function(elem) {
+		return elem.id;
+	});
+
+	for(i=0;i<arrOfId.length;i++){
+		if(arrOfId[i].includes('txt')){
+			var valueOfId = document.getElementById(arrOfId[i]).value;
+			localStorage.setItem(arrOfId[i], valueOfId);
+		}
+	}
+	
+	alert('DATA SAVED LOCALLY.');
+
+}
+
+
+function onLoad(){
+
+	for(i=2;i<12;i++){
+		if(localStorage.getItem('txtskill'+i)){
+			addElm('skill-div');
+		}
+	}
+	for(i=2;i<6;i++){
+		if(localStorage.getItem('txtOther'+i)){
+			addElm('cert-div');
+		}
+	}
+	let htmlids = document.querySelectorAll('*[id]');
+	let arrOfId = Array.prototype.map.call( htmlids, function(elem) {
+		return elem.id;
+	});
+
+	for(i=0;i<arrOfId.length;i++){
+		if(arrOfId[i].includes('txt')){
+			document.getElementById(arrOfId[i]).value = localStorage.getItem(arrOfId[i]);
+		}
+	}
+
+}
+
+
+window.onload = function () { 
+	setTimeout(onLoad, 1000);
+}
+
+
+// localStorage.setItem("lastname", "Smith");
+
+// for Nav Bar
 $(document).ready(function () {
 	$('.mbnav').on('click', function () {
 		$('.navblock').slideToggle();
@@ -64,6 +118,7 @@ $(document).ready(function () {
 });
 
 
+// for Next and previous
 $('.btnNext').click(function(){
 	$('.nav-tabs > .active').next('li').find('a').trigger('click');
 });
